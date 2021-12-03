46136a6b1b374f53b17b77c96483d81c26cc03e1
DesireTilleras <74039931+DesireTilleras@users.noreply.github.com>
2021-12-03 10:48:55 +0100

    Day3

---
+++ Server/day3P1.js
@@ -0,0 +1,60 @@
+let gammaArray = [];
+let epsilonArray = [];
+
+var fs = require("fs");
+fs.readFile("./day3P1.txt", function (err, data) {
+  if (err) throw err;
+  var array = data.toString().split("\n");
+
+  for (j = 0; j < 12; j++) {
+   
+    gammaArray.push(returnMostCommon(j, array));
+  }
+
+  for(x = 0; x < gammaArray.length; x++){
+      if(gammaArray[x] === 1){
+          epsilonArray.push('0')
+      }
+      else if(gammaArray[x] ===0){
+          epsilonArray.push('1')
+      }
+  }
+
+  const gammaString = gammaArray.join('');
+  const epsilonString = epsilonArray.join('');
+
+  const gammaDecimal = parseInt(gammaString,2);
+  const epsilonDecimal = parseInt(epsilonString, 2);
+
+  const answer = gammaDecimal * epsilonDecimal
+
+  console.log(answer);
+
+});
+
+function returnMostCommon(index, array) {
+
+    let count0 = 0;
+    let count1 = 0;
+
+    for(i = 0; i< array.length; i++){
+
+
+        if(array[i].charAt(index) === '1'){
+
+            count1++;
+        }
+        else if(array[i].charAt(index) === '0'){
+            count0++;
+        }
+    }
+
+    let mostCommonNumber;
+
+    if(count1> count0){
+        mostCommonNumber = 1;
+    }
+    else{ mostCommonNumber = 0}
+
+  return mostCommonNumber;
+}
---
+++ Server/day3P2.js
@@ -0,0 +1,93 @@
+let gammaArray = [];
+let epsilonArray = [];
+
+var fs = require("fs");
+fs.readFile("./day3P1.txt", function (err, data) {
+  if (err) throw err;
+  var array = data.toString().split("\n");
+
+let oxygenGenerator = returnMostCommon(0, array);
+let scrubber = returnLeastCommon(0, array);
+
+console.log(oxygenGenerator);
+console.log(scrubber);
+
+
+  const oxygenGeneratorDecimal = parseInt(oxygenGenerator, 2);
+  const scrubberDecimal = parseInt(scrubber, 2);
+
+  const answer = oxygenGeneratorDecimal * scrubberDecimal;
+
+  console.log(answer);
+});
+
+function returnMostCommon(index, array) {
+
+    if(array.length === 1)
+    {return array[0]}
+  let count0 = 0;
+  let count1 = 0;
+
+  for (i = 0; i < array.length; i++) {
+    if (array[i].charAt(index) === "1") {
+      count1++;
+    } else if (array[i].charAt(index) === "0") {
+      count0++;
+    }
+  }
+
+  let mostCommonNumber;
+
+  if (count1 >= count0) {
+    mostCommonNumber = 1;
+  } else {
+    mostCommonNumber = 0;
+  }
+
+  let newArray = [];
+
+  for(y = 0; y < array.length; y++) {
+    if (array[y].charAt(index) == mostCommonNumber) {
+        newArray.push(array[y]);
+    }
+  }
+
+  return returnMostCommon(index + 1, newArray);
+  
+}
+
+
+
+function returnLeastCommon(index, array) {
+  if (array.length === 1) {
+    return array[0];
+  }
+  let count0 = 0;
+  let count1 = 0;
+
+  for (i = 0; i < array.length; i++) {
+    if (array[i].charAt(index) === "1") {
+      count1++;
+    } else if (array[i].charAt(index) === "0") {
+      count0++;
+    }
+  }
+
+  let leastCommonNumber;
+
+  if (count1 < count0) {
+    leastCommonNumber = 1;
+  } else if(count1 >= count0) {
+    leastCommonNumber = 0;
+  }
+
+  let newArray = [];
+
+  for (y = 0; y < array.length; y++) {
+    if (array[y].charAt(index) == leastCommonNumber) {
+      newArray.push(array[y]);
+    }
+  }
+
+  return returnLeastCommon(index + 1, newArray);
+}
