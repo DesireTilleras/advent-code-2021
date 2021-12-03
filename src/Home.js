87a1a839f77ebfad1ddb5e5ae5c4bfc4cc439697
DesireTilleras <74039931+DesireTilleras@users.noreply.github.com>
2021-12-03 13:58:17 +0100

    update-title

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import Image from './Images/tree.png'
 
 const Home = () => {
-    return ( 
-        <div>
-            <img src={Image} alt="iewh" style={{"width" : "550px"}}/>
-        </div>
-     );
+    return (
+      <div style={{ alignItems: "center", display:"flex", flexDirection:"column" }}>
+          <p>Amanda's and Dessi's adventOfCode!</p>
+        <img src={Image} alt="iewh" style={{ width: "550px" }} />
+      </div>
+    );
 }
  
 export default Home;
